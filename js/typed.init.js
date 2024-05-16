3c08927d4cc657dfa1ecde7cb6efadd90866186d
Matthew Adenyo <adenyomatthew@gmail.com>
2024-05-16 04:52:36 +0000

    Add files via upload

---
+++ js/typed.init.js
@@ -0,0 +1,11 @@
+// Typed Text
+var typed = new Typed("#typed", {
+  stringsElement: "#typed-strings",
+  typeSpeed: 200,
+  loop: true,
+  strings: [
+    "Data Analysis",
+    "Mathematics Tutoring",
+    "YouTube Content Creation",
+  ],
+});
